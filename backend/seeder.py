c4099a515e94e3f7e8559295d16d0c7113f52312
guillenec <guillermoneculqueo@gmail.com>
2026-04-18 21:49:13 -0300

    fix(seeder) se actualizan roles en el seeder.

--- backend/seeder.py
+++ backend/seeder.py
@@ -8,14 +8,14 @@ app = create_app()
 
 def seed():
     # Crear roles
-    admin_role = Rol(nombre='superadmin')
-    user_role = Rol(nombre='user')
-    db.session.add_all([admin_role, user_role])
+    admin_role = Rol(nombre='admin')
+    operador_role = Rol(nombre='operador')
+    db.session.add_all([admin_role, operador_role])
     db.session.commit()
 
     # Crear usuarios
     admin_user = User(nombre='admin', password='admin123', rol_id=admin_role.id, email='admin@example.com')
-    regular_user = User(nombre='user', password='user123', rol_id=user_role.id, email='user@example.com')
+    regular_user = User(nombre='operador', password='operador123', rol_id=operador_role.id, email='operador@example.com')
     db.session.add_all([admin_user, regular_user])
     db.session.commit()
     
